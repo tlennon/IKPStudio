4c019f067bfa04357cbb83d39454f84e5e9a7a2f
tlennon <tlennon1@hotmail.com>
2022-03-13 23:39:48 -0400

    Updates

---
+++ components/header.js
@@ -0,0 +1,83 @@
+﻿const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    nav {
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: left;
+      background-color:  #0a0a23;
+    }
+
+    ul {
+      padding: 0;
+    }
+    
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+    
+    a {
+      font-weight: 700;
+      margin: 0 25px;
+      color: #fff;
+      text-decoration: none;
+    }
+    
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #fff;
+    }
+
+
+    h1 {
+        color: blue;
+        text-align: center;
+    }
+
+    h4 {
+        color: black;
+        text-align: center;
+    }
+
+    div {
+        top: 75%;
+        height: 75px;
+        padding: 25px;
+        line-height: 15px;
+        margin-top: -25px;
+
+    }
+
+  </style>
+  <header>
+    <div>
+        <h1>Ivory Key Productions</h1>
+        <h4>Audio Home Music Studio Services</h4>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="studio.html">Studio Pics</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="about.html">About</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+
+        shadowRoot.appendChild(headerTemplate.content);
+    }
+}
+
+customElements.define('header-component', Header);
